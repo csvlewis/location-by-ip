7337060f4c4adefeebadeff4b349be1b1a609b76
Chris Lewis <13chlewis@gmail.com>
2019-10-30 16:59:29 -0600

    Update spec with two ip addresses

--- spec/features/location_by_ip_spec.rb
+++ spec/features/location_by_ip_spec.rb
@@ -5,7 +5,19 @@ describe 'As a user' do
     it 'I can see location information about the given ip' do
       visit('http://localhost:3000/locationByIp?ip=76.25.168.96')
 
-      expect(page).to have_content("'ID': 'US'")
+      expect(page).to have_content('US')
+      expect(page).to have_content('United States')
+      expect(page).to have_content(39.729)
+      expect(page).to have_content(-104.832)
+      expect(page).to have_content('United States - Colorado - Aurora')
+
+      visit('http://localhost:3000/locationByIp?ip=103.252.178.64')
+
+      expect(page).to have_content('AU')
+      expect(page).to have_content('Australia')
+      expect(page).to have_content(-27.469)
+      expect(page).to have_content(153.023)
+      expect(page).to have_content('Australia - Queensland - Brisbane')
     end
   end
 end
